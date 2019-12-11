42bb68c83764c5fbbec296f6b2702083b3222c15
Nielsbishere <n@osomi.net>
2019-12-11 11:04:53 +0100

    Fixed build

--- third_party/src/nuklear/nuklear.cpp
+++ third_party/src/nuklear/nuklear.cpp
@@ -16,7 +16,7 @@
 
 #define NK_IMPLEMENTATION
 
-#include "../nuklear/nuklear.h"
+#include "../../nuklear/nuklear.h"
 #include "system/allocator.hpp"
 #include "types/enum.hpp"
 #include "input/input_device.hpp"
