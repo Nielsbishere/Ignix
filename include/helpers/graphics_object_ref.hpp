2cee0b88210aedf44ec1c2633a665b5c1a0bfaa0
Nielsbishere <n@osomi.net>
2020-03-21 18:13:04 +0100

    Renamed igx helper structs to TRef instead of T so ignis classes can still be used and distinguished and it is clearer which ones are used.

--- include/helpers/graphics_object_ref.hpp
+++ include/helpers/graphics_object_ref.hpp
@@ -18,6 +18,7 @@
 namespace igx {
 
 	using namespace ignis::cmd;
+	using namespace ignis;
 
 	//A wrapper for constructing and destructing graphics objects
 	//It creates a GraphicsObject per name; meaning all names should be unique
@@ -29,7 +30,6 @@ namespace igx {
 
 	public:
 
-		using Info = typename T::Info;
 		using Ptr = T*;
 
 		void release() {
@@ -84,7 +84,7 @@ namespace igx {
 
 		//Creates a graphics object
 		GraphicsObjectRef(
-			ignis::Graphics &g, const String &name, const Info &info
+			ignis::Graphics &g, const String &name, const typename T::Info &info
 		) {
 
 			//Find existing resource
@@ -123,50 +123,18 @@ namespace igx {
 
 	//Graphics
 
-	using Graphics = ignis::Graphics;
-
 	//Managed resources
 
-	using CommandList = GraphicsObjectRef<ignis::CommandList>;
-	using Swapchain = GraphicsObjectRef<ignis::Swapchain>;
-	using Framebuffer = GraphicsObjectRef<ignis::Framebuffer>;
-	using PrimitiveBuffer = GraphicsObjectRef<ignis::PrimitiveBuffer>;
-	using GPUBuffer = GraphicsObjectRef<ignis::GPUBuffer>;
-	using ShaderBuffer = GraphicsObjectRef<ignis::ShaderBuffer>;
-	using Pipeline = GraphicsObjectRef<ignis::Pipeline>;
-	using Texture = GraphicsObjectRef<ignis::Texture>;
-	using Sampler = GraphicsObjectRef<ignis::Sampler>;
-	using Descriptors = GraphicsObjectRef<ignis::Descriptors>;
-
-	//Data for info structs
-
-	using BufferAttributes = ignis::BufferAttributes;
-	using ShaderBufferLayout = ignis::ShaderBuffer::Layout;
-	using BufferLayout = ignis::BufferLayout;
-	using PipelineLayout = ignis::PipelineLayout;
-	using DescriptorsSubresources = ignis::Descriptors::Subresources;
-	using RegisterLayout = ignis::RegisterLayout;
-	using GPUSubresource = ignis::GPUSubresource;
-	using Rasterizer = ignis::Rasterizer;
-	using BlendState = ignis::BlendState;
-	using MSAA = ignis::MSAA;
-	using DepthStencil = ignis::DepthStencil;
-
-	using GPUFormat = ignis::GPUFormat;
-	using DepthFormat = ignis::DepthFormat;
-	using GPUBufferType = ignis::GPUBufferType;
-	using TextureType = ignis::TextureType;
-	using SamplerType = ignis::SamplerType;
-	using ShaderAccess = ignis::ShaderAccess;
-	using ShaderStage = ignis::ShaderStage;
-	using GPUMemoryUsage = ignis::GPUMemoryUsage;
-	using PipelineFlag = ignis::Pipeline::Flag;
-	using TopologyMode = ignis::TopologyMode;
-	using FillMode = ignis::FillMode;
-	using CullMode = ignis::CullMode;
-	using WindMode = ignis::WindMode;
-	using BlendLogicOp = ignis::BlendState::LogicOp;
-	using Stencil = ignis::DepthStencil::Stencil;
+	using CommandListRef = GraphicsObjectRef<ignis::CommandList>;
+	using SwapchainRef = GraphicsObjectRef<ignis::Swapchain>;
+	using FramebufferRef = GraphicsObjectRef<ignis::Framebuffer>;
+	using PrimitiveBufferRef = GraphicsObjectRef<ignis::PrimitiveBuffer>;
+	using GPUBufferRef = GraphicsObjectRef<ignis::GPUBuffer>;
+	using ShaderBufferRef = GraphicsObjectRef<ignis::ShaderBuffer>;
+	using PipelineRef = GraphicsObjectRef<ignis::Pipeline>;
+	using TextureRef = GraphicsObjectRef<ignis::Texture>;
+	using SamplerRef = GraphicsObjectRef<ignis::Sampler>;
+	using DescriptorsRef = GraphicsObjectRef<ignis::Descriptors>;
 
 }
 
